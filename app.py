0e9b1e2ad7e1c6f0236ad680c7ff34aad9ff7e97
jainchirag <jainc3620@gmail.com>
2025-08-04 15:36:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ st.title("📚 Bookstore Scraper App")
 st.dataframe(df)
 
 # Price filter
-price_limit = st.slider("Show books below this price (£):", 0, 100, 30)
+price_limit = st.slider("Show books below this price (₹):", 0, 100, 30)
 filtered = df[df['Price'] <= price_limit]
-st.write(f"### Books under £{price_limit}")
+st.write(f"### Books under ₹{price_limit}")
 st.dataframe(filtered)
 
 # Rating filter
 rating = st.selectbox("Select minimum rating:", [0, 1, 2, 3, 4, 5])
 filtered_rating = df[df['Rating'] >= rating]
 st.write(f"### Books with rating ≥ {rating}")
+
 st.dataframe(filtered_rating)
